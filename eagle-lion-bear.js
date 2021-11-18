b415ead1b89f5d1ec48b551cbc5ce6aeb993c337
imbrandonj <imbrandonj42@gmail.com>
2021-11-18 16:10:09 -0600

    Update

--- eagle-lion-bear.js
+++ eagle-lion-bear.js
@@ -6,7 +6,7 @@ const selection = ["Eagle", "Lion", "Bear"];
     W/ return value "Hello <username>" 
     Explain the game */
 
-const username = prompt("Hello Player, what's your name?");
+let username = prompt("Hello Player, what's your name?");
 alert("Hello " + username);
 alert("Let's play a classic: Rock Paper Scissors.\n\nExcept this variation is called Eagle Lion Bear");
 alert("Eagle < Lion < Bear < Eagle");
@@ -14,62 +14,99 @@ alert("Best out of 5 wins!");
 
 console.log("Hello " + username);
 
-/* Restarted games begin here */
+/* Restarted games begin here 
 
-/* Player is prompted for option
-    "You choose:"
-    "Eagle Lion or Bear?"
-    Answer is recorded */
+function game() { } */ 
 
-const playerSelection = prompt("You choose:\n\nEagle Lion or Bear?");
-console.log(playerSelection);
 
-/* It's the computer's turn */
+let userWins = 0;
+let computerWins = 0;
 
-function computerPlay(selection) {
-    return selection[Math.floor(Math.random() * selection.length)];
+// Figure out looping all of the below together.
+
+while ((userWins < 3) && (computerWins < 3)) {   
+
+    /* Player is prompted for option
+        "You choose:"
+        "Eagle Lion or Bear?"
+        Answer is recorded */
+
+        const playerSelection = prompt("You choose:\n\nEagle Lion or Bear?");
+        console.log(playerSelection);
+
+
+        /* It's the computer's turn */
+
+        function computerPlay(selection) {
+            return selection[Math.floor(Math.random() * selection.length)];
+        }
+
+        const computerSelection = computerPlay(selection);
+
+        alert("Computer chooses " + computerSelection);
+        console.log(computerSelection);
+
+        /* Compare selections and choose winner 
+            This is the calculateWinner function*/
+
+        function calculateWinner(playerSelection, computerSelection) {
+            if (playerSelection === "Eagle") {
+                if (computerSelection === "Bear") {
+                    userWins++;
+                    return username + " wins!";
+                } else if (computerSelection === "Lion") {
+                    computerWins++;
+                    return "Computer wins!";
+                } else {
+                    return "It's a tie!";
+                }
+            } if (playerSelection === "Lion") {
+                if (computerSelection === "Eagle") {
+                    userWins++;
+                    return username + " wins!";
+                } else if (computerSelection === "Bear") {
+                    computerWins++;
+                    return "Computer wins!";
+                } else {
+                    return "It's a tie!";
+                    }
+            } if (playerSelection === "Bear") {
+                if (computerSelection === "Lion") {
+                    userWins++;
+                    return username + " wins!";
+                } else if (computerSelection === "Eagle") {
+                    computerWins++;
+                    return "Computer wins!";
+                } else {
+                    return "It's a tie!";
+                }
+            } 
+        } // end calculateWinner function
+
+    const outcome = calculateWinner(playerSelection, computerSelection);
+    alert(outcome);
+    console.log(outcome);
+    alert(username + " " + userWins + " vs Computer " + computerWins); 
+    console.log(username + " " + userWins + " vs Computer " + computerWins);
+
+    // Begin next game of 5 
 }
 
-const computerSelection = computerPlay(selection);
+    // OR
+    // Declare Winner of 5 
 
-alert("Computer chooses " + computerSelection);
-console.log(computerSelection);
-
-/* Compare selections and choose winner */
-
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == "eagle") && (computerSelection === "Bear")) {
-        return result = username + " wins!";
-    } else if ((playerSelection == "eagle") && (computerSelection === "Eagle")) {
-        return result = "It's a tie!";
-    } else if ((playerSelection == "eagle") && (computerSelection === "Lion")) {
-        return result = "Computer wins!";
-    } else if ((playerSelection == "lion") && (computerSelection === "Eagle")) {
-        return result = username + " wins!";
-    } else if ((playerSelection == "lion") && (computerSelection === "Lion")) {
-        return result = "It's a tie!";
-    } else if ((playerSelection == "lion") && (computerSelection === "Bear")) {
-        return result = "Computer wins!";
-    } else if ((playerSelection == "bear") && (computerSelection === "Lion")) {
-        return result = username + " wins!";
-    } else if ((playerSelection == "bear") && (computerSelection === "Bear")) {
-        return result = "It's a tie!";
-    } else if ((playerSelection == "bear") && (computerSelection === "Eagle")) {
-        return result = "Computer wins!";
+  if (userWins === 3) {
+        alert(username + " wins best of five!");
+    } else { 
+        alert("The Computer wins best of five!");
     }
-}
-   
-
-const outcome = playRound(playerSelection, computerSelection);
-alert(outcome);
-console.log(outcome);
-
-/* Begin next game of 5 */
-
-/* Declare Winner of 5 */
+    
 
 /* Ask, "play again?"
     If yes, start over
-    If no, clear page, "thanks for playing" */
+    Loop */
+
+
+    // If no, End Loop, "thanks for playing" */
 
     alert("Thanks for playing.");
